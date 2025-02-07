b482d23695661a1dd5592672d1f69524b5940338
inessa <inessa220@mail.ru>
2025-02-07 17:02:34 +0600

    correction function sort by date

--- src/processing.py
+++ src/processing.py
@@ -10,5 +10,5 @@ def filter_by_state(list_of_dictionary: list, choose_state: str = "EXECUTED") ->
 
 def sort_by_date(list_of_dictionary: list, sort: bool = True) -> list:
     """Функция, принимающая список словарей, сортирующая по дате, по умолчанию - убывание"""
-    sorted_list = sorted(list_of_dictionary, key = lambda x: x["date"], reverse = sort)
+    sorted_list = sorted(list_of_dictionary, key=lambda i: i["date"], reverse=sort)
     return sorted_list
