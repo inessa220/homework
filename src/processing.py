c6bfc60aff4b79b2da1c5c1891aa4c2089e1276e
inessa <inessa220@mail.ru>
2025-02-07 13:09:11 +0600

    fuction filter_by_state and sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -8,4 +8,7 @@ def filter_by_state(list_of_dictionary: list, choose_state: str = "EXECUTED") ->
     return new_list_of_dictionary
 
 
-
+def sort_by_date(list_of_dictionary: list, sort: bool = True) -> list:
+    """Функция, принимающая список словарей, сортирующая по дате, по умолчанию - убывание"""
+    sorted_list = sorted(list_of_dictionary, key = lambda x: x["date"], reverse = sort)
+    return sorted_list
